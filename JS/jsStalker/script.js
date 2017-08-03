10d38c655dbc14884d11f48ec1772873e0e92cf6
Jon Marshall <jonathan.marshall6@gmail.com>
2017-08-03 19:43:09 -0400

    javascript projects

---
+++ JS/jsStalker/script.js
@@ -0,0 +1,6 @@
+var usrFirstName = prompt("What is your first name?");
+var usrLastName = prompt("What is your last name?");
+var usrAge = prompt("What is your age?");
+
+console.log("Your name is " + usrFirstName + " " + usrLastName);
+console.log("Your age is " + usrAge);
