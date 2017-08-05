83d2bab61fd16d60a7bd6deb20ef50f0c19e1fe8
Jon Marshall <jonathan.marshall6@gmail.com>
2017-08-05 02:48:52 -0400

    refactored todoList with functions

--- JS/todoList/todo.js
+++ JS/todoList/todo.js
@@ -2,22 +2,44 @@ var todos = [];
 
 var input = prompt("What would you like to do?");
 
+// main app
 while(input !== "quit"){
-
     if(input === "list"){
-        console.log(todos);
+        listTodos();
     }
-
-    // check for input commands
     else if(input === "new"){
-        // ask for new todo
-        var newTodo = prompt("Enter new todo")
-
-        // add todos to array
-        todos.push(newTodo);
+        newTodos();
     }
-
+    else if(input === "delete"){
+        deleteTodos();
+    }
     // ask again for new input
     input = prompt("What would you like to do?");
 }
 console.log("OK, You quit the app");
+
+// functions
+function listTodos(){
+    console.log("**********");
+    todos.forEach(function(todo, i){
+        console.log(i + ": " + todo);
+    });
+    console.log("**********");
+}
+
+function newTodos(){
+    // ask for new todo
+    var newTodo = prompt("Enter new todo")
+
+    // add todos to array
+    todos.push(newTodo);
+    console.log("Added todo");
+}
+
+function deleteTodos(){
+    // ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    // delete that todo
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+}
